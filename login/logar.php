0f09da15a0e33facdaffe4c767adc16c3e33ff49
ojoaovictor1 <ojoaovictor@live.com>
2026-02-20 15:08:12 -0300

    novo front

--- login/logar.php
+++ login/logar.php
@@ -13,8 +13,8 @@ curl_setopt_array($curl, [
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_POST => true,
     CURLOPT_POSTFIELDS => json_encode([
-        'username' => $usuario,
-        'password' => $senha
+        'email' => $usuario,
+        'senha'=> $senha
     ]),
     CURLOPT_HTTPHEADER => [
         'Content-Type: application/json'
@@ -24,6 +24,7 @@ curl_setopt_array($curl, [
 $response = curl_exec($curl);
 
 if (curl_errno($curl)) {
+    echo 'chegou no primeiro errono';
     die('Erro na requisição: ' . curl_error($curl));
 }
 
@@ -40,4 +41,26 @@ if(isset($json['token'])) {
     exit();
 }
 
+// $env = parse_ini_file(__DIR__ . '/../config/.env');
+// $url_buscar = $env['API_URL'] . '/usuarios';
+// //buscar usuarios
+
+// $curl_buscar = curl_init();
+// curl_setopt_array($curl_buscar, [
+//     CURLOPT_URL => $url_buscar,
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_HTTPHEADER => [
+//         "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJqb2FvQGdtYWlsLmNvbSIsImlhdCI6MTc3MTU5MDk1OSwiZXhwIjoxNzcxNTk0NTU5fQ.WnSIE7tdPId-hiqAOu85wE9Q5pVzIUbLU6A-fP3tjsA",
+//         'Content-Type: application/json'
+//     ]
+// ]);
+
+// $response_buscar = curl_exec($curl_buscar);
+
+// if (curl_errno($curl_buscar)) {
+//     die('Erro na requisição: ' . curl_error($curl_buscar));
+// }
+
+// $json_buscar = json_decode($response_buscar, true);
+// echo json_encode($json_buscar[1]);
 ?>
